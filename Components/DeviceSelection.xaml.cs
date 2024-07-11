e18bb3e1cbe6c46cbef3f09046c69cd719c96922
Luiz FELIX <luiz.felix@al-enterprise.com>
2024-07-11 09:47:14 -0400

    device type selection

---
+++ Components/DeviceSelection.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace PoEWizard.Components
+{
+    /// <summary>
+    /// Interaction logic for DeviceSelection.xaml
+    /// </summary>
+    public partial class DeviceSelection : Window
+    {
+        public List<string> DeviceTypes { get;  set; } = new List<string>() { "Access Point", "Camera", "Telephone", "Other" };
+        public string Device { get; set; }
+
+        public DeviceSelection(string port)
+        {
+            InitializeComponent();
+            DataContext = this;
+            _header.Text = "Please, selecte the type of device conntect to port " + port;
+        }
+    }
+}
