e9c0372df9280f171b40a5a6aff2e2ec3062a07a
Luiz FELIX <luiz.felix@al-enterprise.com>
2024-08-14 15:36:07 -0400

    created struct CmdRequest

--- Data/Utils.cs
+++ Data/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -14,9 +15,24 @@ using System.Windows;
 using System.Xml;
 using System.Xml.Linq;
 using static PoEWizard.Data.Constants;
+using static PoEWizard.Data.RestUrl;
 
 namespace PoEWizard.Data
 {
+    
+    public struct CmdRequest
+    {
+        public CmdRequest(Command command, CommandType type, string[] data)
+        {
+            Command = command;
+            Type = type;
+            Data = data;
+        }
+        public Command Command { get; }
+        public CommandType Type { get; }
+        public string[] Data { get; }
+    }
+
     public static class Utils
     {
         private const string ENCRYPT_KEY = "a9cd76210f6e0bb4fdbd23a9cda9831a";
