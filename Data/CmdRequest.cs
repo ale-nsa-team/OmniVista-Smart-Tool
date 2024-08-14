d2dc49b9ad1858bb7400e38db8013498a8187194
Luiz FELIX <luiz.felix@al-enterprise.com>
2024-08-14 16:04:27 -0400

    replaced struct CmdRequest with class

--- Data/CmdRequest.cs
+++ Data/CmdRequest.cs
@@ -3,17 +3,20 @@ using static PoEWizard.Data.RestUrl;
 
 namespace PoEWizard.Data
 {
-    public struct CmdRequest
+    public class CmdRequest
     {
+        public CmdRequest(Command command) : this(command, ParseType.Text, null, 1) { }
+        public CmdRequest(Command command, ParseType type) : this(command, type, null, 1) { }
+        public CmdRequest(Command command, ParseType type, int nbHeaders) : this(command, type, null, nbHeaders) { }
         public CmdRequest(Command command, ParseType type, string[] data, int nbHeaders)
         {
             Command = command;
-            Type = type;
+            ParseType = type;
             Data = data;
             NbHeaders = nbHeaders;
         }
         public Command Command { get; }
-        public ParseType Type { get; }
+        public ParseType ParseType { get; }
         public string[] Data { get; }
         public int NbHeaders { get; }
     }
