d017f730ab5bead29695ecd18d19f0407a0a500f
jalmeida <joao.almeida@al-enterprise.com>
2024-09-09 10:19:45 -0400

    - Adding modified values to "ConfigChanges" class.

--- Data/ConfigChanges.cs
+++ Data/ConfigChanges.cs
@@ -11,6 +11,7 @@ namespace PoEWizard.Data
         public SwitchModel SwitchModel { get; set; }
         public Dictionary<string, List<string>> AddedChanges { get; set; }
         public Dictionary<string, List<string>> RemovedChanges { get; set; }
+        public Dictionary<string, List<string>> ModifiedChanges { get; set; }
 
         private int nbLines = 0;
 
@@ -19,26 +20,18 @@ namespace PoEWizard.Data
             this.SwitchModel = switchModel;
             this.AddedChanges = new Dictionary<string, List<string>>();
             this.RemovedChanges = new Dictionary<string, List<string>>();
+            this.ModifiedChanges = new Dictionary<string, List<string>>();
             Dictionary<string, List<string>> currConfig = CliParseUtils.ParseSwitchConfigChanges(SwitchModel.ConfigSnapshot);
             Dictionary<string, List<string>> prevConfig = CliParseUtils.ParseSwitchConfigChanges(prevCfgSnapshot);
             foreach (KeyValuePair<string, List<string>> keyVal in currConfig)
             {
                 List<string> currList = keyVal.Value;
-                List<string> addedChanges = new List<string>();
-                List<string> removedChanges = new List<string>();
                 if (prevConfig.ContainsKey(keyVal.Key))
                 {
                     List<string> prevList = prevConfig[keyVal.Key];
-                    if (currList.Count > prevList.Count) addedChanges = currList.Except(prevList).ToList();
-                    else removedChanges = prevList.Except(currList).ToList();
-                }
-                if (addedChanges.Count > 0)
-                {
-                    this.AddedChanges[keyVal.Key] = addedChanges;
-                }
-                if (removedChanges.Count > 0)
-                {
-                    this.RemovedChanges[keyVal.Key] = removedChanges;
+                    if (currList.Count == prevList.Count) this.ModifiedChanges[keyVal.Key] = currList.Except(prevList).ToList();
+                    else if (currList.Count > prevList.Count) this.AddedChanges[keyVal.Key] = currList.Except(prevList).ToList();
+                    else this.RemovedChanges[keyVal.Key] = prevList.Except(currList).ToList();
                 }
             }
         }
@@ -46,8 +39,9 @@ namespace PoEWizard.Data
         public override string ToString()
         {
             this.nbLines = 0;
-            Dictionary<string, StringBuilder> featureChanges = GetChanges(this.AddedChanges, false, new Dictionary<string, StringBuilder>());
-            featureChanges = GetChanges(this.RemovedChanges, true, featureChanges);
+            Dictionary<string, StringBuilder> featureChanges = GetChanges(this.AddedChanges, "Added", new Dictionary<string, StringBuilder>());
+            featureChanges = GetChanges(this.RemovedChanges, "Removed", featureChanges);
+            featureChanges = GetChanges(this.ModifiedChanges, "Modified", featureChanges);
             StringBuilder text = new StringBuilder();
             foreach (KeyValuePair<string, StringBuilder> keyVal in featureChanges)
             {
@@ -56,19 +50,13 @@ namespace PoEWizard.Data
             return text.ToString();
         }
 
-        private Dictionary<string, StringBuilder> GetChanges(Dictionary<string, List<string>> cfgChanges, bool removed, Dictionary<string, StringBuilder> featureChanges)
+        private Dictionary<string, StringBuilder> GetChanges(Dictionary<string, List<string>> cfgChanges, string action, Dictionary<string, StringBuilder> featureChanges)
         {
             foreach (KeyValuePair<string, List<string>> keyVal in cfgChanges)
             {
+                if (keyVal.Value.Count < 1) continue;
                 StringBuilder txt;
-                if (featureChanges.ContainsKey(keyVal.Key))
-                {
-                    txt = featureChanges[keyVal.Key];
-                }
-                else
-                {
-                    txt = new StringBuilder();
-                }
+                if (featureChanges.ContainsKey(keyVal.Key)) txt = featureChanges[keyVal.Key]; else txt = new StringBuilder();
                 txt.Append("\n - ").Append(keyVal.Key).Append(":");
                 this.nbLines++;
                 if (this.nbLines >= MAX_NB_LINES_CHANGES_DISPLAYED)
@@ -76,12 +64,9 @@ namespace PoEWizard.Data
                     txt.Append(":\n                     . . .");
                     break;
                 }
-                List<string> changes = keyVal.Value;
-                foreach (string change in changes)
+                foreach (string change in keyVal.Value)
                 {
-                    txt.Append("\n   ");
-                    if (removed) txt.Append("Removed: "); else txt.Append("Added: ");
-                    txt.Append(change);
+                    txt.Append("\n    ").Append(action).Append(": ").Append(change);
                     this.nbLines++;
                     if (this.nbLines >= MAX_NB_LINES_CHANGES_DISPLAYED)
                     {
