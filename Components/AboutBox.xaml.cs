a6672bd98104eebdce8fc46b8fda63f8785291e4
Luiz FELIX <luiz.felix@al-enterprise.com>
2024-09-09 11:58:56 -0400

    disclamer in about box

--- Components/AboutBox.xaml.cs
+++ Components/AboutBox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using static PoEWizard.Data.Constants;
@@ -10,6 +11,19 @@ namespace PoEWizard.Components
     /// </summary>
     public partial class AboutBox : Window
     {
+        private string description = "This application allows you to troubleshoot PoE issues on an Alcatel-Lucent Enterprise OmniSwitch®, " +
+            "equipped with AOS 8 version.\nThe application communicates with the switch via REST API, to gather information on the power " +
+            "supplies and PoE ports, and allows the user to perform some configuration changes to mitigate common PoE issues. " +
+            "In case the wizard is unable to fix the problem, it allows the user to collect relevant information to be sent to TAC.\r\n";
+
+        private string disclamer = "\r\nPermission to use, copy, modify, and distribute this source code, product, " +
+            "and its documentation without a fee and without a signed license agreement is hereby granted, provided that the copyright notice, this " +
+            "paragraph, and the following two paragraphs appear in all copies, modifications, and distributions." +
+            "\r\n \r\nIN NO EVENT SHALL ALE USA INC. BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING " +
+            "LOST PROFITS, ARISING OUT OF THE USE OF THIS SOURCE CODE, PRODUCT, AND ITS DOCUMENTATION, EVEN IF ALE USA INC. HAS BEEN ADVISED OF THE " +
+            "POSSIBILITY OF SUCH DAMAGE.\r\n \r\nALE USA INC. SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTES " +
+            "OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOURCE CODE, PRODUCT, AND ACCOMPANYING DOCUMENTATION, IF ANY, " +
+            "PROVIDED HEREUNDER IS PROVIDED “AS IS.” ALE USA INC. HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.\r\n";
         public AboutBox()
         {
             InitializeComponent();
@@ -22,16 +36,16 @@ namespace PoEWizard.Components
                 Resources.MergedDictionaries.Remove(Resources.MergedDictionaries[1]);
 
             }
+
+            int year = DateTime.Now.Year;
             Assembly assembly = Assembly.GetExecutingAssembly();
             string version = assembly.GetName().Version.ToString();
             _aboutTitle.Text = assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
             _version.Text = "Version " + string.Join(".", version.Split('.').ToList().Take(2));
             _company.Text = assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
-            _copyWright.Text = assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright; ;
-            _aboutDescr.Text = "This application allows you to troubleshoot PoE issues on an Alcatel-Lucent Enterprise OmniSwitch®, equipped with AOS 8 version.\n" +
-                "The application communicates with the switch via REST API, to gather information on the power supplies and PoE ports," +
-                " and allows the user to perform some configuration changes to mitigate common PoE issues.\n" +
-                "In case the wizard is unable to fix the problem, it allows the user to collect relevant information to be sent to TAC.";
+            _copyRight.Text = $"Copyright © {year} ALE USA Inc. All Rights Reserved.";
+            _aboutDescr.Text = description + disclamer;
+
         }
 
         private void BtnOk_Click(object sender, RoutedEventArgs e)
