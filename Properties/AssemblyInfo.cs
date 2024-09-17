db9e2e33537df97eff2d2e847f7c381253de170f
Luiz FELIX <luiz.felix@al-enterprise.com>
2024-09-17 11:15:30 -0400

    version update to 0.41

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.41.0.0")]
-[assembly: AssemblyFileVersion("0.40.1.0")]
+[assembly: AssemblyFileVersion("0.41.0.0")]
