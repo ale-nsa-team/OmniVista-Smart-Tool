5d8cacc68f5fbd4b3b0590415cf4a4dc16c8001d
jalmeida <joao.almeida@al-enterprise.com>
2024-10-02 08:35:07 -0400

    - Adding a delay of 500ms before sending the linux command "df -h" in "UpdateFlashInfo" method of "RestApiService" class. - Modifying "SearchMac.xaml" class to search for MAXC address or vendor name.

--- Comm/RestApiService.cs
+++ Comm/RestApiService.cs
@@ -165,6 +165,7 @@ namespace PoEWizard.Comm
                             LinuxCommandSeq cmdSeq;
                             if (chassis.IsMaster) cmdSeq = new LinuxCommandSeq();
                             else cmdSeq = new LinuxCommandSeq(new LinuxCommand($"ssh-chassis {SwitchModel.Login}@{chassis.Number}", "Password|Are you sure", 20));
+                            Thread.Sleep(500);
                             cmdSeq.AddCommandSeq(new List<LinuxCommand> { new LinuxCommand("su", "->"), new LinuxCommand("df -h", "->"), new LinuxCommand("exit", sessionPrompt) });
                             cmdSeq = SendSshLinuxCommandSeq(cmdSeq, $"Reading Flash data of chassis {chassis.Number}");
                             _dict = cmdSeq?.GetResponse("df -h");
--- Components/SearchMac.xaml.cs
+++ Components/SearchMac.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PoEWizard.Device;
+﻿using PoEWizard.Data;
+using PoEWizard.Device;
 using System;
 using System.Collections.ObjectModel;
 using System.Windows;
@@ -12,8 +13,11 @@ namespace PoEWizard.Components
     public partial class SearchPort : Window
     {
 
+        private string _device_mac = string.Empty;
+
         public ObservableCollection<PortModel> PortsFound { get; set; }
         public PortModel SelectedPort { get; set; }
+        public bool IsMacAddress {  get; set; }
 
         public SearchPort(SwitchModel device, string macAddress)
         {
@@ -28,14 +32,16 @@ namespace PoEWizard.Components
                 Resources.MergedDictionaries.Remove(Resources.MergedDictionaries[1]);
             }
             SelectedPort = null;
-            PortsFound = SearchMacAddress(device, macAddress);
+            SearchMacAddress(device, macAddress);
             if (PortsFound.Count == 1) SelectedPort = PortsFound[0];
         }
 
-        private ObservableCollection<PortModel> SearchMacAddress(SwitchModel device, string macAddress)
+        private void SearchMacAddress(SwitchModel device, string macAddr)
         {
-            if (!string.IsNullOrEmpty(macAddress)) macAddress = macAddress.ToLower().Trim();
-            ObservableCollection<PortModel> portsFound = new ObservableCollection<PortModel>();
+            this._device_mac = !string.IsNullOrEmpty(macAddr) ? macAddr.ToLower().Trim() : string.Empty;
+            string[] splitMac = this._device_mac.Split(':');
+            this.IsMacAddress = string.IsNullOrEmpty(this._device_mac) || (splitMac.Length > 1 && !string.IsNullOrEmpty(splitMac[1]));
+            this.PortsFound = new ObservableCollection<PortModel>();
             foreach (var chas in device.ChassisList)
             {
                 foreach (var slot in chas.Slots)
@@ -44,29 +50,47 @@ namespace PoEWizard.Components
                     {
                         if (port.EndPointDevicesList?.Count > 0)
                         {
-                            if (!string.IsNullOrEmpty(port.EndPointDevicesList[0].MacAddress))
+                            if (FoundDevice(port))
                             {
-                                if (port.EndPointDevicesList[0].MacAddress.Contains(macAddress) && !portsFound.Contains(port))
-                                {
-                                    portsFound.Add(port);
-                                    if (port.MacList?.Count == 0) port.MacList.Add(port.EndPointDevicesList[0].MacAddress);
-                                    continue;
-                                }
+                                this.PortsFound.Add(port);
+                                if (port.MacList?.Count == 0) port.MacList.Add(port.EndPointDevicesList[0].MacAddress);
+                                continue;
                             }
                         }
                         if (port.MacList?.Count == 0) continue;
                         foreach (string mac in port.MacList)
                         {
-                            if (mac.Contains(macAddress) && !portsFound.Contains(port))
+                            if (FoundDevice(port, mac))
                             {
-                                portsFound.Add(port);
+                                this.PortsFound.Add(port);
                                 break;
                             }
                         }
                     }
                 }
             }
-            return portsFound;
+        }
+
+        private bool FoundDevice(PortModel port, string macAddr = null)
+        {
+            string mac = string.Empty;
+            if (string.IsNullOrEmpty(macAddr))
+            {
+                if (port.EndPointDevicesList?.Count > 0)
+                {
+                    if (!this.IsMacAddress)
+                    {
+                        mac = (!string.IsNullOrEmpty(port.EndPointDevicesList[0].Vendor) ? port.EndPointDevicesList[0].Vendor :
+                               Utils.GetVendorName(port.EndPointDevicesList[0].MacAddress)).ToLower();
+                    }
+                    else mac = port.EndPointDevicesList[0].MacAddress;
+                }
+            }
+            else
+            {
+                if (!this.IsMacAddress) mac = Utils.GetVendorName(macAddr);
+            }
+            return mac.Contains(this._device_mac) && !this.PortsFound.Contains(port);
         }
 
         private void PortSelection_Changed(Object sender, RoutedEventArgs e)
