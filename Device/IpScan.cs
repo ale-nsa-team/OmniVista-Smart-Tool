9fefd6726572120c50c2108facf7933ca8fb907e
Luiz FELIX <luiz.felix@al-enterprise.com>
2025-02-06 18:34:31 -0500

    loop waiting for python script to finish loading

--- Device/IpScan.cs
+++ Device/IpScan.cs
@@ -3,6 +3,7 @@ using PoEWizard.Data;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PoEWizard.Device
@@ -11,7 +12,7 @@ namespace PoEWizard.Device
     {
         private const string PING_SCRIPT = "PoEWizard.Resources.ping.py";
         private const string REM_PATH = Constants.PYTHON_DIR + "ping.py";
-        private const int SCAN_TIMEOUT = 4*60*1000;
+        private const int SCAN_TIMEOUT = 4 * 60 * 1000;
         private static SwitchModel model;
         private static SftpService sftpSrv;
 
@@ -24,8 +25,18 @@ namespace PoEWizard.Device
                 if (err != null) throw new Exception(err);
                 using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(PING_SCRIPT))
                 {
-                    if (sftpSrv.IsConnected && sftpSrv.UploadStream(resource, REM_PATH, true))
+                    if (sftpSrv.IsConnected)
                     {
+                        long filesize = 0L;
+                        int count = 0;
+                        sftpSrv.UploadStream(resource, REM_PATH, true);
+                        while (filesize < resource.Length && count < 3)
+                        {                          
+                            Thread.Sleep(2000);
+                            filesize = sftpSrv.GetFileSize(REM_PATH);
+                            count++;
+                        }
+
                         sftpSrv.Disconnect();
                         await RunScript();
                     }
@@ -55,6 +66,7 @@ namespace PoEWizard.Device
                 {
                     ssh.ConnectSshClient();
                     Dictionary<string, string> resp = ssh.SendCommand(new RestUrlEntry(Command.RUN_PYTHON_SCRIPT, new string[] { REM_PATH }), SCAN_TIMEOUT);
+                    if (resp["output"].Contains("Err")) Logger.Error($"Failed to run ip scan on switch {model.Name}: {resp["output"]}");
                     resp = ssh.SendCommand(new RestUrlEntry(Command.SHOW_ARP), null);
                     ssh.DisconnectSshClient();
                     ssh.Dispose();
@@ -70,7 +82,7 @@ namespace PoEWizard.Device
                     Logger.Error($"Timeout waiting for ip address scan on switch {model.Name}");
                 }
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Logger.Error($"Could not scan IP addresses on switch {model.Name}", ex);
             }
