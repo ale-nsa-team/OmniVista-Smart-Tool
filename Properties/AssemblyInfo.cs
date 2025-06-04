6b15e5067179729f9dffa59dc519db36bfda732c
lv-ho <lvho@tma.com.vn>
2025-06-04 18:49:34 +0700

    Add WindowsSetup installer project, update solution/project metadata - Add WindowsSetup .vdproj installer to solution - Update solution to include installer project and remove x64 configs - Change AssemblyName to "AOS Toolkit" and update application icon in .csproj - Add EULA.rtf to project resources - Ensure language folder exists before file copy in MainWindow - Update AssemblyTitle for branding consistency

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Alcatel Lucent Enterprise Installer's Toolkit")]
+[assembly: AssemblyTitle("Alcatel-Lucent Enterprise Installer's Toolkit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alcatel-Lucent Enterprise")]
