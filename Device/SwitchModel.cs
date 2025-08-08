f9eae53e726c02f7ecc33adff0649e53f66c3af3
lv-ho <lvho@tma.com.vn>
2025-08-08 09:12:46 +0700

    CRVMS240: Gate U-Boot upgrade by ONIE support; add tooltip and i18n
    - Add Utils.IsNoneOniePlatform(model) with OnieSupportedModels list (Data/Utils.cs)
    - Expose SwitchModel.IsNoneOniePlatform (Device/SwitchModel.cs)
    - Bind _ubootUpgrade IsEnabled and add tooltip via IsNoneOniePlatformToTooltipConverter (MainWindow.xaml, Components/ValueConverters.cs)
    - Register converter in ResourceDictionary-{dark,light}.xaml
    - Add i18n key i18n_noUbootSupported (Resources/strings-enUS.xaml)
    - Make Translate null-safe with MainWindow.Strings? (Data/Utils.cs)

--- Device/SwitchModel.cs
+++ Device/SwitchModel.cs
@@ -58,6 +58,8 @@ namespace PoEWizard.Device
         public int LpCmmDebugLevel => GetAppLogLevel(LPCMM);
         public string LpCmmLabelDebugLevel => GetLabelAppLogLevel(LPCMM);
 
+        public bool IsNoneOniePlatform => Utils.IsNoneOniePlatform(Model);
+
         public SwitchModel() : this("", DEFAULT_USERNAME, DEFAULT_PASSWORD, SWITCH_CONNECT_TIMEOUT_SEC) { }
 
         public SwitchModel(string ipAddr, string username, string password, int cnxTimeout)
